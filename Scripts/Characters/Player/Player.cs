38c6ac943477e9a32c6450fb5893218d2f1d6a12
Phoenix Elliott <phoenix.elliott19@outlook.com>
2024-04-25 21:17:10 +0100

    Playing animations and Game Constants

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -3,7 +3,16 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    [ExportGroup("Required Nodes")]
+    [Export] private AnimationPlayer animationPlayer;
+    [Export] private Sprite3D sprite3D;
+
     private Vector2 direction = new();
+
+    public override void _Ready()
+    {
+        animationPlayer.Play(GameConstants.ANIM_IDLE);
+    }
 
     public override void _PhysicsProcess(double delta)
     {
@@ -15,6 +24,20 @@ public partial class Player : CharacterBody3D
 
     public override void _Input(InputEvent @event)
     {
-        direction = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
+        direction = Input.GetVector(
+            GameConstants.INPUT_MOVE_LEFT,
+            GameConstants.INPUT_MOVE_RIGHT,
+            GameConstants.INPUT_MOVE_FORWARD,
+            GameConstants.INPUT_MOVE_BACKWARD
+        );
+
+        if (direction == Vector2.Zero)
+        {
+            animationPlayer.Play(GameConstants.ANIM_IDLE);
+        }
+        else
+        {
+            animationPlayer.Play(GameConstants.ANIM_MOVE);
+        }
     }
 }
